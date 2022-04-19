87e1543a28f35a252a2b62c3f30459e120a93f8d
zhangyue <yueyue_freedom@163.cm>
2022-04-19 17:52:37 +0800

    debug_to_false

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wsl2_b_t!aru=atgjbl%!fhoic9-7z_%zx&be**pl4ygmy$y$c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
